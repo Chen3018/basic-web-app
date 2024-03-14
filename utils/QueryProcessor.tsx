4b4232a2d25d609ef1d4c2ee93f8e84d973ecbc2
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-03-14 15:54:02 -0400

    Added name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,7 +15,7 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("name")) {
     return (
-      "My name is Chenjia Fan."
+      "My name is chenjiaf"
     );
   }
 
