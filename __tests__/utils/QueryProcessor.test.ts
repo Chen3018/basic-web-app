4bbf6842e668f734324e0c3ff2c2c7bccda79105
Chen3018 <chenjiafan3018@gmail.com>
2024-08-30 22:42:47 -0400

    I need tests

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,2 +1,9 @@
-import QueryProcessor from "../../utils/QueryProcessor";
+import CalculatePrice from "../../utils/QueryProcessor";
 import '@testing-library/jest-dom'
+
+describe("CalculatePrice", () => {
+    test("should return a number", () => {
+        const response: number = CalculatePrice([], 0, 0);
+        expect(typeof response).toBe("number");
+    });
+});
